24d525709c2455c51730e0b7b581008a174a933d
jenniferbusca <jennifer.busca@gmail.com>
2019-07-18 13:38:13 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -31,5 +31,4 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'capybara'
   gem 'database_cleaner'
-  gem 'pry'
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,30 +1,3 @@
-# class StudentsController < ApplicationController
-#   before_action :set_student, only: :show
-#   def index
-#     @students = Student.all
-#   end
-#
-#   def show
-#   end
-#
-#   # def create
-#   #   session[:form_params] = params.inspect
-#   #   redirect_to new_student_path
-#   # end
-#
-#   def new
-#   end
-#
-#   def create
-#     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-#     redirect_to new_student_path
-#   end
-# private
-#
-#   def set_student
-#     @student = Student.find(params[:id])
-#   end
-# end
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
 
@@ -36,16 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    # @student = Student.new
   end
 
   def create
-    session[:form_params] = params.inspect
-    redirect_to new_student_path
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to student_path(@student)
   end
 
   private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
